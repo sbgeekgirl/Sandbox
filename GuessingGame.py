d9b94e63469e4601082bed5a711161265b39f2fc
Amy Tate <you@example.com>
2026-01-28 15:33:46 -0700

    first commit

---
+++ GuessingGame.py
@@ -0,0 +1,38 @@
+import random
+
+x = random.randint(1, 101)
+print(f'The number is {x}')
+guess_string = input('Guess a number between 1 - 100: ')
+guess = int(guess_string)
+
+if guess < 1 or guess > 100:
+    print('OUT OF BOUNDS!')
+elif guess == x:
+    print('You guessed correctly! It only took you one try!')
+elif (x - 11) > guess < (x + 11):
+    print('WARM')
+else:
+    print('COLD')
+
+counter = 1
+
+while x != guess:
+    old_guess_distance = abs(guess - x)
+    guess_string = input('Guess a number between 1 - 100: ')
+    guess = int(guess_string)
+    guess_distance = abs(guess - x)     
+    counter += 1
+    if guess < 1 or guess > 100:
+        print('OUT OF BOUNDS!')
+    elif guess == x:
+        print(f'You guessed correctly! It only took you {counter} tries!')
+    elif guess_distance <= old_guess_distance:
+        print('WARMER')
+    else:
+        print('COLDER')
+    
+
+
+
+
+
